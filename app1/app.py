aa2b128a05c80d3caff1294b404b688a7dc6cfa9
Petr <petr@hruzik.net>
2026-02-27 21:53:19 +0100

    Oprava preklepu v requirements3

--- app1/app.py
+++ app1/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Ahoj! Moje prvni GitOps aplikace bezi!</h1>"
+    return "<h1>Ahoj! Moje druha GitOps aplikace bezi!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
